bec1e7c1cc3ff3452d361e73be0cd735c0270c37
val-cox <johnrally847@gmail.com>
2025-11-09 21:41:48 +0100

    Allow Render domain in Vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: "./",
-});
+  preview: {
+    allowedHosts: ['rehearsalmanager-axn9.onrender.com'],
+  },
+})
