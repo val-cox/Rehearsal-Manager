6c06b47083fe99a56106bdbaa7b11b16354e24bc
val-cox <johnrally847@gmail.com>
2025-11-09 21:50:18 +0100

    Update CORS config to allow frontend on Render

--- backend/src/main/java/com/valentino/rehearsalmanager/config/WebConfig.java
+++ backend/src/main/java/com/valentino/rehearsalmanager/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://rehearsal-manager-frontend.onrender.com") 
+                        .allowedOrigins("http://localhost:5173", "https://rehearsalmanager-axn9.onrender.com") 
            
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
